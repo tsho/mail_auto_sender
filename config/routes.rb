f73d603ecd47172a54faa93313b47a45a5145c05
henry <sadmear@gmail.com>
2014-03-18 15:50:33 +0900

    add root route for Themes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 MailAutoSender::Application.routes.draw do
 
 resources :themes
+rout 'themes#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
