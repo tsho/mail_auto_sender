eb8e0ffa2fc43d2fe8c67522a8a1a8043cdbe601
sho <twinkle.over.the.sky@gmail.com>
2014-03-18 14:03:17 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MailAutoSender::Application.config.secret_key_base = 'e69452f303a6a5172a4a0aac42b7b473f335561a1a8d7089128a34eb56bce1d9ce8a00197402bb83a85e5f4a3dca8c9c724f5749b7b0106c5940f3d4309df15c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MailAutoSender::Application.config.session_store :cookie_store, key: '_mail_auto_sender_session'
