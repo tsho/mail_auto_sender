12810608c7b2cad7b4c0bd9cf604ab519c52d62a
sho <twinkle.over.the.sky@gmail.com>
2014-03-23 18:52:44 +0900

    Created load .yml & sending email via gmail at port number of 587

--- app/mailers/test_mailer.rb
+++ app/mailers/test_mailer.rb
@@ -2,7 +2,9 @@ class TestMailer < ActionMailer::Base
   default from: "from@example.com"
 
   def send_henry_mail
+#    options = File.join(Rails.root, 'config', '"#{RAILS_ROOT}/config/gmail_info.yml')
     mail to: "sadmear@gmail.com", suject: "Test"
   end
+
 
 end
